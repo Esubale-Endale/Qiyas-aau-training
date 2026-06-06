452a940b1effc443557c9c42514ec1f1b19054dc
Esubale Endale <esu1011esu@gmail.com>
2026-06-05 23:27:23 -0700

    matterials

--- tms-client/models/course.model.ts
+++
@@ -1,8 +0,0 @@
-import type { Temporal } from "@js-temporal/polyfill";
-
-export interface Course {
-  readonly id: string;
-  title: string;
-  capacity: number;
-  startDate?: Temporal.PlainDate;
-}
